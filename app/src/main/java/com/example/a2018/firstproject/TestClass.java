97775450f67b5c4d9fa5e4a45f7ac6fa71df8fb8
osama <medo1442002@yahoo.com>
2018-10-28 04:30:15 +0300

    this is the first commit

---
+++ app/src/main/java/com/example/a2018/firstproject/TestClass.java
@@ -0,0 +1,18 @@
+package com.example.a2018.firstproject;
+
+import java.util.ArrayList;
+
+public class TestClass {
+    public static void main(String []arg){
+
+        ArrayList<Integer>myArrayList=new ArrayList<>();
+        myArrayList.add(1);
+        myArrayList.add(3);
+        myArrayList.add(5);
+
+        System.out.print(myArrayList.get(0)
+        );
+
+
+    }
+}
